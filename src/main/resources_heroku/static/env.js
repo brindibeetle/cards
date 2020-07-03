75d9b1630078ae60f29c9858841028b41812d51b
brindibeetle <brindi.beetle@gmail.com>
2020-07-03 15:34:09 +0200

    Redesign of PlayRequest en Response

---
+++ src/main/resources_heroku/static/env.js
@@ -0,0 +1,4 @@
+var flags='\
+    {\
+        "backend_base_url"     :   "https://jokeren-emile.herokuapp.com/cards-ws" \
+    }'
---
+++ src/main/resources_localhost/static/env.js
@@ -0,0 +1,4 @@
+var flags='\
+    {\
+        "backend_base_url"     :   "http://localhost:8080/cards-ws" \
+    }'
